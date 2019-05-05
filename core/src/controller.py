cfc0e505564322b40f2a186b07adb1914f31e539
gabylan12 <gabriel.radikal@gmail.com>
2019-05-05 18:58:12 -0300

    add model image

--- core/src/controller.py
+++ core/src/controller.py
@@ -9,6 +9,12 @@ import time
 
 # Configure the PIN # 8
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 GPIO.setup(36, GPIO.OUT)
 GPIO.setup(38, GPIO.OUT)
 GPIO.setup(40, GPIO.OUT)
@@ -24,6 +30,13 @@ def right():
 
 @app.route("/stop")
 def stop():
+    GPIO.output(11, False)
+    GPIO.output(13, False)
+    GPIO.output(16, False)
+    GPIO.output(18, False)
+    GPIO.output(36, False)
+    GPIO.output(38, False)
+    GPIO.output(40, True)
     return "Stop"
 
 @app.route("/left")
@@ -32,29 +45,35 @@ def left():
 
 @app.route("/forward")
 def forward():
+    GPIO.output(16, True)
+    GPIO.output(18, False)
+    GPIO.output(20, True)
     return "Forward"
 
 @app.route("/backward")
 def backward():
+    GPIO.output(16, False)
+    GPIO.output(18, True)
+    GPIO.output(26, True)
     return "Backward"
 
 @app.route("/green")
 def green():
-    GPIO.output(36, True)
-    GPIO.output(38, False)
+    GPIO.output(36, False)
+    GPIO.output(38, True)
     GPIO.output(40, False)
     return "Green"
 
 @app.route("/blue")
 def blue():
-    GPIO.output(36, False)
-    GPIO.output(38, True)
+    GPIO.output(36, True)
+    GPIO.output(38, False)
     GPIO.output(40, False)
     return "Blue"
 
 @app.route("/yellow")
 def yellow():
-    GPIO.output(36, False)
-    GPIO.output(38, False)
-    GPIO.output(40, True)
+    GPIO.output(36, True)
+    GPIO.output(38, True)
+    GPIO.output(40, False)
     return "Yellow"
